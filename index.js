43f256f38fc7b2b70e061af263057c1329637df6
Prince Thind <princethind799@gmail.com>
2021-07-03 19:29:45 +0530

    refactor code

--- index.js
+++ index.js
@@ -4,27 +4,12 @@ const fs = require('fs');
 http
   .createServer(function (req, res) {
     const url = req.url;
-    let content = null;
-
-    switch (url) {
-      case '/':
-      case '/index.html':
-        content = load('./html/index.html');
-        break;
-      case '/contact-me.html':
-        content = load('./html/contact-me.html');
-        break;
-      case '/about.html':
-        content = load('./html/about.html');
-        break;
-      default:
-        content = load('./html/404.html');
-        break;
-    }
-    console.log(url);
-    return content;
+    load('.' + url);
 
     function load(file) {
+      const fileExist = checkExistence(file);
+      if (!fileExist) file = './404.html';
+
       fs.readFile(file, function (err, data) {
         if (err) {
           console.log(err);
@@ -32,8 +17,22 @@ http
         }
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
-        return res.end();
+        res.end();
       });
+    }
+
+    function checkExistence(file) {
+      let fileExist = false;
+      switch (file) {
+        case './index.html':
+        case './about.html':
+        case './contact-me.html':
+          fileExist = true;
+          break;
+        default:
+          fileExist = false;
+      }
+      return fileExist;
     }
   })
   .listen(8080);
