8a80198562bed789a7f0365c783b372876af8b4a
Prince Thind <princethind799@gmail.com>
2021-07-04 13:49:30 +0530

    rewrite code with express framework

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
-const http = require('http');
 const fs = require('fs');
+const express = require('express');
+const app = express();
+const port = 8000;
 
-http
-  .createServer(function (req, res) {
-    const url = req.url;
-    load('.' + url);
+app.get('/', (req, res) => {
+  const content = load('./index.html');
+  res.send(content);
+});
 
-    function load(file) {
-      const fileExist = checkExistence(file);
-      if (!fileExist) file = './404.html';
+app.get('/about', (req, res) => {
+  const content = load('./about.html');
+  res.send(content);
+});
+app.get('/contact-me', (req, res) => {
+  const content = load('./contact-me.html');
+  res.send(content);
+});
 
-      fs.readFile(file, function (err, data) {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        res.end();
-      });
-    }
+app.listen(port, () => {
+  console.log('listening on port:' + port);
+});
 
-    function checkExistence(file) {
-      let fileExist = false;
-      switch (file) {
-        case './index.html':
-        case './about.html':
-        case './contact-me.html':
-          fileExist = true;
-          break;
-        default:
-          fileExist = false;
-      }
-      return fileExist;
-    }
-  })
-  .listen(8080);
+function load(file) {
+  const data = fs.readFileSync(file, { encoding: 'utf8', flag: 'r' });
+  return data;
+}
+
+// function checkExistence(file) {
+//   let fileExist = false;
+//   switch (file) {
+//     case './index.html':
+//     case './about.html':
+//     case './contact-me.html':
+//       fileExist = true;
+//       break;
+//     default:
+//       fileExist = false;
+//   }
+//   return fileExist;
+// }
